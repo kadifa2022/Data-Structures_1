4d58f18bebe4a665af076c81bb67ac8bf48453f6
Kadifa <Kudickadifa@gmail.com>
2022-09-09 12:50:19 -0500

    Linked List Demo

--- _04inkedLists/src/main/java/com/Cydeo/MySinglyLinkedList.java
+++ _04inkedLists/src/main/java/com/Cydeo/MySinglyLinkedList.java
@@ -1,6 +1,38 @@
 package com.Cydeo;
 
 public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){
+        return head ==null;
+    }
+    void add (int data){
+        //create a new node object from data
+        Node node = new Node(data);
+        if (isEmpty()){// if the list is empty
+            head=tail=node;
+            size++;
+        }else {
+            tail.next=node;
+            tail=node;
+            size++;
+        }
+    }
+    void printNodes(){
+        Node current=head;
+        while (current !=null) {
+            if (current.next == null) System.out.println(current.id + "=>null");
+            else {
+                System.out.println(current.id + "=>");
+            }
+
+            current = current.next;
+        }
+    }
+
+
 
 
 }
