93d9d7d523938ff44f31e5d3531650e56bc0eaf6
Kadifa <Kudickadifa@gmail.com>
2022-09-09 20:21:34 -0500

    Linked List Demo

--- _04inkedLists/src/main/java/com/Cydeo/LinkedListDemo.java
+++ _04inkedLists/src/main/java/com/Cydeo/LinkedListDemo.java
@@ -7,9 +7,9 @@ public class LinkedListDemo {
         Node node3=new Node(3);
         Node node4=new Node(4);
 
-        node1.next=node2;
-        node2.next=node2;
-        node3.next=node2;
+        node1.next=node2;//each one has a different address
+        node2.next=node3;
+        node3.next=node4;
 
         System.out.println(node1.id);
 
