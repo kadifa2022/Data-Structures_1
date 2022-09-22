6f20e85a7f7a86c1d710159d467056bcce9af686
Kadifa <Kudickadifa@gmail.com>
2022-09-21 23:25:35 -0500

    practice

--- _04inkedLists/src/main/java/com/Cydeo/practice/IsPalindrome.java
+++ _04inkedLists/src/main/java/com/Cydeo/practice/IsPalindrome.java
@@ -4,26 +4,49 @@ import java.util.Locale;
 
 public class IsPalindrome {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("anavolimilovana"));
+        System.out.println(isPalindrome2("levelP"));
 
     }
 
-    public static boolean isPalindrome(String word){
-        if(word.isEmpty() || word ==null){
+
+    public static boolean isPalindrome(String word) {
+        if (word.isEmpty() || word == null) {
             return false;
         }
         word = word.replace(" ", "").toLowerCase();
-        int left =0;
-        int right=word.length()-1;
-        while(left<right){
-            if(word.charAt(left) ==word.charAt(right)){
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) == word.charAt(right)) {
                 right--;
                 left++;
-            }else{
+            } else {
                 return false;
             }
         }
         return true;
     }
+
+    public static boolean isPalindrome2(String word) {
+        if (word.isEmpty() || word == null) {
+            return false;
+        }
+        word = word.replace("", "").toLowerCase();
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if(word.charAt(left)==word.charAt(right)){
+                right--;
+                left ++;
+            }else{
+                return false;
+            }
+
+        }
+        return true;
+    }
+
 
 
 }
