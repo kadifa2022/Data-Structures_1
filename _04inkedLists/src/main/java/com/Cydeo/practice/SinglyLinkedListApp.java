a6d63cd3f97b09eb4eb2865f438bf752d26df076
Kadifa <Kudickadifa@gmail.com>
2022-09-21 23:26:09 -0500

    practice

---
+++ _04inkedLists/src/main/java/com/Cydeo/practice/SinglyLinkedListApp.java
@@ -0,0 +1,21 @@
+package com.Cydeo.practice;
+
+public class SinglyLinkedListApp {
+
+    public static void main(String[] args) {
+
+
+        MySinglyLinkedList myList1=new MySinglyLinkedList();
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+
+
+
+
+    }
+
+
+}
